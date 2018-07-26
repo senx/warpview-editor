e3d65326ff4b76889ce627e2e6654d2e0a24d18c
Xavier Marin <marin.xavier@gmail.com>
2018-07-26 14:14:33 +0200

    initial commit

---
+++ dist/quantum-editor/components.esm.js
@@ -0,0 +1,75 @@
+
+var QuantumEditorComponent = /** @class **/ (function() {
+  function QuantumEditor() {
+  }
+  QuantumEditor.is = 'quantum-editor';
+  QuantumEditor.getModule = function(opts) {
+    
+    return import('./p6zsevtl.js').then(function(m) {
+        return m.QuantumEditor;
+      });
+
+  }
+});
+
+var QuantumResultComponent = /** @class **/ (function() {
+  function QuantumResult() {
+  }
+  QuantumResult.is = 'quantum-result';
+  QuantumResult.getModule = function(opts) {
+    
+    return import('./p6zsevtl.js').then(function(m) {
+        return m.QuantumResult;
+      });
+
+  }
+});
+
+var StcTabContentComponent = /** @class **/ (function() {
+  function StcTabContent() {
+  }
+  StcTabContent.is = 'stc-tab-content';
+  StcTabContent.getModule = function(opts) {
+    
+    return import('./p6zsevtl.js').then(function(m) {
+        return m.StcTabContent;
+      });
+
+  }
+});
+
+var StcTabHeaderComponent = /** @class **/ (function() {
+  function StcTabHeader() {
+  }
+  StcTabHeader.is = 'stc-tab-header';
+  StcTabHeader.getModule = function(opts) {
+    
+    return import('./p6zsevtl.js').then(function(m) {
+        return m.StcTabHeader;
+      });
+
+  }
+});
+
+var StcTabsComponent = /** @class **/ (function() {
+  function StcTabs() {
+  }
+  StcTabs.is = 'stc-tabs';
+  StcTabs.getModule = function(opts) {
+    
+    return import('./p6zsevtl.js').then(function(m) {
+        return m.StcTabs;
+      });
+
+  }
+});
+
+export {
+  
+  QuantumEditor,
+  QuantumResult,
+  StcTabContent,
+  StcTabHeader,
+  StcTabs,
+};
+  
