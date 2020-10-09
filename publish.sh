747b30c92e32931fc237e4d41fd0979a9cfade0f
xavier <marin.xavier@gmail.com>
2020-10-09 17:29:53 +0200

    fix build

--- publish.sh
+++ publish.sh
@@ -2,4 +2,6 @@ yarn version --new-version $1
 (cd projects/warpview-editor-ng && yarn version --new-version $1 )
 yarn clean
 yarn build
+mkdir -p dist/warpview-editor/base/browser/ui/codicons/codicon
+cp node_modules/monaco-editor/dev/vs/base/browser/ui/codicons/codicon/codicon.ttf dist/warpview-editor/base/browser/ui/codicons/codicon/.
 (cd dist/warpview-editor && yarn publish --new-version $1 )
